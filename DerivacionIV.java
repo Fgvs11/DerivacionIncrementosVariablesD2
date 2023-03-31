334d41595979c0f2f35275cb7986c3aa8335b8f4
Fgvs-PC <fgvizcainos@gmail.com>
2023-03-31 09:21:44 -0600

    Funcion completada

--- DerivacionIV.java
+++ DerivacionIV.java
@@ -1,3 +1,9 @@
 public class DerivacionIV{
-	
+	//Funcion 
+    public double f(double x, double xa,double xb,double xc,double ya,double yb,double yc){
+        double part1 = (2*x-xb-xc)/((xa-xb)*(xa-xc)) * ya;
+        double part2 = (2*x-xa-xc)/((xb-xa)*(xb-xc)) * yb;
+        double part3 = (2*x-xb-xa)/((xc-xb)*(xc-xa)) * yc;
+        return part1 + part2 + part3;
+    }
 }
